b81687bd10491f8c7c2589885513fdf724d89620
Deepak Singh <dv72276@gmail.com>
2021-09-10 21:36:59 +0530

    First Commit

---
+++ UICode/UICode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UICode
+//
+//  Created by Deepak Singh on 10/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UICode/UICode/UICodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICodeApp.swift
+//  UICode
+//
+//  Created by Deepak Singh on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct UICodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
